d91af78d3f2ddc4adb3af14a9ca6c7975314031a
Daniel <184745138@qq.com>
2019-05-27 16:20:22 +0100

    Add files via upload

---
+++ src/store/modules/userInfo.js
@@ -0,0 +1,55 @@
+export default {
+    namespaced: true,
+    state: {
+        isSucceed: false,
+        uid: null,
+    },
+    mutations: {
+        updateUserInfo(state, payload) {
+            state.uid = payload.uid
+        },
+
+        updateLoginStatus(state, payload) {
+            state.isSucceed = payload.isSucceed
+        }
+
+    },
+    actions: {
+        login({ dispatch, commit, rootState }) {
+            axios
+                .get(rootState.api.login, {
+                    params: {
+                        phone: '',
+                        password: ''
+                    },
+                    withCredentials: true
+                })
+                .then(function (response) {
+                    const res = response.data;
+                    // console.log(res)
+                    if (res.code === 200) {
+                        commit('updateLoginStatus', { isSucceed: true })
+                        commit('updateUserInfo', { uid: res.account.id });
+                    }
+                    // dispatch('songList/getLikeList', {}, { root: true })
+                });
+        },
+
+        // refreshLoginState() {
+        //     let that = this;
+        //     axios
+        //         .get(this.baseURL + "/login/refresh")
+        //         .then(function(response) {
+        //             // handle success
+        //             console.log(response.data);
+        //         })
+        //         .catch(function(error) {
+        //             // handle error
+        //             console.log(error);
+        //         })
+        //         .finally(function() {
+        //             // always executed
+        //         });
+        // }
+    }
+}
